a616cbc93d02d8f33c2c1f681d310caffdaca674
branden-eyk <brandeneyk@gmail.com>
2023-02-13 18:56:45 -0500

    Add basic functionality to + button and refine CSS positioning

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,14 @@
 const myLibrary = [];
+const addBtn = document.querySelector('.library__card--add');
+
+addBtn.addEventListener('click', handleAdd);
+
+function handleAdd() {
+  const library = document.querySelector('.library');
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('library__card');
+  library.insertAdjacentElement('afterbegin', newDiv);
+}
 
 function Book(title, author, pages, read) {
   this.title = title;
